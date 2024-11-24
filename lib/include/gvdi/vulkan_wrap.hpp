a2f280c89ecaf78bc684d7d8db29c7fa0b1e53ec
Karn Kaul <karnkaul@gmail.com>
2024-11-23 20:37:45 -0800

    [GCC] Suppress third-party warnings

---
+++ lib/include/gvdi/vulkan_wrap.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#if defined(__GNUG__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#endif
+
+#include <vulkan/vulkan.hpp>
+
+#if defined(__GNUG__)
+#pragma GCC diagnostic pop
+#endif
